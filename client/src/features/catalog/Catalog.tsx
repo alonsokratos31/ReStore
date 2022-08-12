627ef74948864de4fb2a655c3577a297f46f56bb
alonso.garcia025 <alonso.garcia025@gmail.com>
2022-08-12 13:09:45 -0400

    End of section 5

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product"
 import ProducList from "./ProductList";
 
@@ -6,14 +8,18 @@ import ProducList from "./ProductList";
 export default function Catalog(){
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     // Si no colocamos el [] el efecto se ejecutara cada vez que el componente se ejecute
     // hay que colocarlo, para que no se vuelva a ejecutar en un loop eterno
     useEffect(() => {
-      fetch('http://localhost:5000/api/Products')
-        .then(response => response.json())
-        .then(data => setProducts(data))
-    }, []);
+      agent.Catalog.list()
+        .then(products => setProducts(products))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) return <LoadingComponent message="Loading Products"/>
   
     return(
         <>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import {  Router } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-  </BrowserRouter>
+
+
+//export const history = createBrowserHistory();
+export const history = createBrowserHistory(); 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router history={history}> 
+    <App />
+    </Router>
+  </React.StrictMode>,
+ document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
